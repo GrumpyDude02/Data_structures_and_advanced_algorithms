9c6d12c70a232b926f9cb8d4016e20e9a2faf3aa
Youssef Bakouch <youssefbakouch02@gmail.com>
2022-09-28 21:28:41 +0100

    binary tree search and traversal

--- binary_tree/binary_tree.c
+++ binary_tree/binary_tree.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct node
 {
@@ -25,7 +26,7 @@ void insert_node(int val, node *HEAD)
         else
         {
             node *new_n = (node *)malloc(sizeof(node));
-            new_n = HEAD->left;
+            new_n = (HEAD)->left;
             new_n->left = NULL;
             new_n->right = NULL;
             insert_node(val, new_n);
@@ -44,12 +45,69 @@ void insert_node(int val, node *HEAD)
         else
         {
             node *new_node = (node *)malloc(sizeof(node));
-            new_node = HEAD->right;
+            new_node = (HEAD)->right;
             new_node->left = NULL;
             new_node->right = NULL;
             insert_node(val, new_node);
         }
     }
+}
+
+bool search(node *HEAD, int val)
+{
+    if (HEAD == NULL)
+    {
+        printf("%d not found\n", val);
+        return false;
+    }
+    if (HEAD->data == val)
+    {
+        printf("%d found\n", val);
+        return true;
+    }
+    if (HEAD->data < val)
+    {
+        return search(HEAD->right, val);
+    }
+    if (HEAD->data > val)
+    {
+        return search(HEAD->left, val);
+    }
+    printf("%d not found\n", val);
+    return false;
+}
+
+void in_order_traversal(node *HEAD)
+{
+    if (HEAD != NULL)
+    {
+        in_order_traversal(HEAD->left);
+        printf("%d\t", HEAD->data);
+        in_order_traversal(HEAD->right);
+    }
+    return;
+}
+
+void pre_order_traversal(node *HEAD)
+{
+    if (HEAD != NULL)
+    {
+        printf("%d\t", HEAD->data);
+        in_order_traversal(HEAD->left);
+        in_order_traversal(HEAD->right);
+    }
+    return;
+}
+
+void post_order_traversal(node *HEAD)
+{
+    if (HEAD != NULL)
+    {
+        in_order_traversal(HEAD->left);
+        in_order_traversal(HEAD->right);
+        printf("%d\t", HEAD->data);
+    }
+    return;
 }
 
 int main()
@@ -60,7 +118,6 @@ int main()
     head->right = NULL;
 
     insert_node(2, head);
-    insert_node(2, head);
     insert_node(0, head);
     insert_node(-1, head);
 
@@ -68,5 +125,12 @@ int main()
     node *temp = (node *)malloc(sizeof(node));
     temp = head->right;
     printf("%d\n", temp->data);
+    search(head, 5);
+    in_order_traversal(head);
+    printf("\n");
+    pre_order_traversal(head);
+    printf("\n");
+    post_order_traversal(head);
+
     return 0;
 }
